31725817f84980351469356ea38f5596e631a075
Anh Pham <phamquocanh2580@gmail.com>
2026-08-24 02:55:29 -0400

    Optimize user and associated setting creation in auth.py

--- server/app/auth.py
+++ server/app/auth.py
@@ -1,6 +1,6 @@
 # Authentication with Steam API. Will be mock data first.
 
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 from fastapi import Depends, HTTPException
 
 from app import models
@@ -16,18 +16,25 @@ def get_current_user(db: Session = Depends(get_db)) -> models.User:
     if not settings.DEV_STEAM_ID:
         raise HTTPException(status_code = 500, detail = "DEV_STEAM_ID not set in .env file.")
 
-    user = db.query(models.User).filter(models.User.steam_id == settings.DEV_STEAM_ID).first()
+    # Eager load the UserSettings associated with this user (done in 1 query).
+    user = db.query(models.User).options(joinedload(models.User.settings)).filter(models.User.steam_id == settings.DEV_STEAM_ID).first()
+
     if not user:
-        # Create a new user
+        # If no user, create user and settings in a single transaction.
         user = models.User(
             steam_id = settings.DEV_STEAM_ID,
             username = settings.DEV_USERNAME,
             avatar_url = "",
+            settings = models.UserSettings() # making use of relationship specified in models.py to automatically set the foreign key (user_id).
         )
 
-        user.settings = models.UserSettings() # making use of relationship specified in models.py to automatically set the foreign key (user_id).
-
         db.add(user)
+        db.commit()
+        db.refresh(user)
+
+    elif not user.settings:
+        # In case a user exists but somehow does not have settings.
+        user.settings = models.UserSettings()
         db.commit()
         db.refresh(user)
     
